dd5ec01ffb61406c196c6e48a91f83a66c83d1ae
Xeal <alexandre-blanchet94@hotmail.com>
2016-01-26 17:22:07 -0500

    Cursor
    Adding a cursor prejab with it's own script and both of his assets given
    by Fulla.

---
+++ assets/scripts/CursorObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorObject : MonoBehaviour
+{
+    public Texture2D cursorTexture1;
+    public Texture2D cursorTexture2;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+    void Start()
+    {
+        Cursor.SetCursor(cursorTexture1, hotSpot, cursorMode); // initialise default state of cursor
+    }
+    void Update()
+    {
+        if (Input.GetMouseButton(0)) // When clicking, depending on current state, change the state
+        {
+            Cursor.SetCursor(cursorTexture2, hotSpot, cursorMode);
+        } else
+        {
+            Cursor.SetCursor(cursorTexture1, hotSpot, cursorMode);
+        }
+    }
+}
