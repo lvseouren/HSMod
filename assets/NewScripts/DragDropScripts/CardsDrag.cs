23263399b98ce4e8a1a1ad7f136b768d68bcc20e
Stanislas Juery <Nightlucidity@gmail.com>
2016-02-07 14:28:32 +0000

    Drag&Drop
    Just new files and more drag and drop is reuploaded

---
+++ assets/NewScripts/DragDropScripts/CardsDrag.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class CardsDrag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+	
+	public Transform yourHand = null;
+	public Transform cardDropZone = null;
+
+	public GameObject token;
+	public GameObject tokenHolder = null;
+
+	public bool selfDestruct = false;
+	
+	public void OnBeginDrag(PointerEventData eventData) {
+		tokenHolder = new GameObject("PlaceHolder");
+		tokenHolder.transform.SetParent( transform.parent );
+		tokenHolder.transform.SetSiblingIndex( transform.GetSiblingIndex() );
+
+		LayoutElement _layout = tokenHolder.AddComponent<LayoutElement>();
+		_layout.preferredWidth = GetComponent<LayoutElement>().preferredWidth;
+
+		yourHand = transform.parent;
+		cardDropZone = yourHand;
+		transform.SetParent( transform.parent.parent );
+		
+		GetComponent<CanvasGroup>().blocksRaycasts = false;
+	}
+		
+	public void OnDrag(PointerEventData eventData) {
+
+		transform.position = eventData.position;
+
+		if(tokenHolder.transform.parent != cardDropZone)
+			tokenHolder.transform.SetParent(cardDropZone);
+
+		int _newSiblingIndex = cardDropZone.childCount;
+
+		for(int i=0; i < cardDropZone.childCount; i++) {
+			if(transform.position.x < cardDropZone.GetChild(i).position.x) {
+
+				_newSiblingIndex = i;
+
+				if(tokenHolder.transform.GetSiblingIndex() < _newSiblingIndex)
+					_newSiblingIndex--;
+
+				break;
+			}
+		}
+
+		tokenHolder.transform.SetSiblingIndex(_newSiblingIndex);
+
+	}
+	
+	public void OnEndDrag(PointerEventData eventData) {
+		transform.SetParent( yourHand );
+		transform.SetSiblingIndex( tokenHolder.transform.GetSiblingIndex() );
+
+		GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+		Destroy(tokenHolder);
+		if(selfDestruct) {
+			Destroy (gameObject);
+		}
+	}
+}
