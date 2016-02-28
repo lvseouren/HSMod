5ec7dc4c44961b65948eb3f932d80050aaba46b2
ChickenDash12 <a.folda@arcor.de>
2016-02-28 14:39:09 +0100

    Added files via upload

---
+++ assets/NewScripts/BasicMinionSuperClass.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicMinionSuperClass : MonoBehaviour {
+
+
+	public int baseHealth ;
+	public int baseAttack ;
+	public int currentAttack ;
+	public int currentHealth ;
+	public int manaCost ;
+	public string TAG ;
+
+	public bool hasDivinieShield ;
+	public bool hasWindfury ;
+	public bool hasStealth ;
+	public bool hasCharge ;
+    public bool hasMegaWindfury ;
+    public bool hasMegaDivineShield;
+
+	public bool hasTargetAbility ;
+	public bool canTargetAllies ;
+	public bool canTargetEnemies ;
+	public bool canTargetHeroes ;
+	public bool isFrozen ;
+
+	void OnEnable () {
+
+	}
+
+	void OnDisable () {
+		// stop listening the methods
+
+	}
+
+	public virtual void OnSummon () {
+
+	}
+
+	public virtual void OnBattleCry () {
+		BattleCry[] btcries =  (BattleCry[]) GetComponents<BattleCry> ();
+		foreach ( BattleCry bc in btcries ){
+			bc.OnBattleCry () ;
+		}
+	}
+
+	public virtual void OnDeathRattle () {
+
+	}
+
+}
