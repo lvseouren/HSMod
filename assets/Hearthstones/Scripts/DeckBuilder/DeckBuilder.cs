d9c8819774f1f8406a45019ae474c8eb36f96928
Robert Piwowarek <robpiwowarek@gmail.com>
2016-03-25 19:14:15 +0100

    Fix&CleanUP attempt

---
+++ assets/Hearthstones/Scripts/DeckBuilder/DeckBuilder.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeckBuilder : MonoBehaviour {
+
+    public GameObject[] Collection;
+    public GameObject[] DisplayedCards = new GameObject[8];
+    public GameObject[] Deck = new GameObject[30];
+
+    // starting positions for showing cards
+    // currently on scene
+    // x = -7 and Y = 4
+    // spawn other cards on 5 x axis difference and 8 y axis difference
+    private const int _firstCardXCoordinate = -7;
+    private const int _firstCardYCoordinate = 4;
+
+    void Start()
+    {
+        // assuming we already got collection from somewhere
+        TESTING_ONLY_FILL_ARRAY();
+       
+        if (Collection != null)
+        {
+            Display();
+
+        }
+
+    }
+
+    void TESTING_ONLY_FILL_ARRAY()
+    {
+        if (Collection == null){
+            Debug.LogError("TESTING ONLY ERROR");
+            return;
+        }
+
+        for (int i = 0; i < 8; i++)
+        {
+            DisplayedCards[i] = Collection[0];
+        }
+    }
+
+    public void AddToDeck()
+    {
+
+    }
+
+    void Display()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            int _newX = _firstCardXCoordinate + (i * 5)%20;
+            int _newY = _firstCardYCoordinate - ((int)i / 4) * 8;
+            Instantiate(DisplayedCards[i], new Vector3(_newX,_newY, 0f), Quaternion.identity);
+        }
+    }
+
+    void DisplayNext()
+    {
+        
+    }
+
+    void DisplayPrev()
+    {
+
+    }
+
+    void ClearDisplayedCards()
+    {
+        if (DisplayedCards == null)
+            return;
+
+        for (int i = 0; i < 8; i++)
+        {
+            if (DisplayedCards[i] != null)
+            {
+                GameObject.Destroy(DisplayedCards[i], 0f);
+                DisplayedCards[i] = null;
+            }
+        }
+    }
+
+    // one day we will implement this
+    // to get our collection from somewhere
+    /*
+    GameObject[] GetCollection()
+    {
+
+    }
+    */
+
+
+}
--- assets/Hearthstones/Scripts/Interactions/BasicMinionSuperClass.cs
+++ assets/Hearthstones/Scripts/Interactions/BasicMinionSuperClass.cs
@@ -1,31 +1,31 @@
-<<<<<<< HEAD:assets/Hearthstones/Scripts/Interactions/BasicMinionSuperClass.cs
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class BasicMinionSuperClass : MonoBehaviour {
+public class BasicMinionSuperClass : MonoBehaviour
+{
 
-	public int baseHealth ;
-	public int baseAttack ;
-	public int currentAttack ;
-	public int currentHealth ;
-	public int baseManaCost ;
+    public int baseHealth;
+    public int baseAttack;
+    public int currentAttack;
+    public int currentHealth;
+    public int baseManaCost;
     public int currentManaCost;
-	public string TAG ;
+    public string TAG;
 
     public bool hasSpellImmunity;
-	public bool hasDivinieShield ;
-	public bool hasWindfury ;
-	public bool hasStealth ;
-	public bool hasCharge ;
+    public bool hasDivinieShield;
+    public bool hasWindfury;
+    public bool hasStealth;
+    public bool hasCharge;
     public bool hasTaunt;
     public bool hasMegaWindfury;
     public bool hasMegaDivineShield;
 
-	public bool hasTargetAbility ;
-	public bool canTargetAllies ;
-	public bool canTargetEnemies ;
-	public bool canTargetHeroes ;
-	public bool isFrozen ;
+    public bool hasTargetAbility;
+    public bool canTargetAllies;
+    public bool canTargetEnemies;
+    public bool canTargetHeroes;
+    public bool isFrozen;
 
     void OnEnable()
     {
@@ -42,7 +42,7 @@ public class BasicMinionSuperClass : MonoBehaviour {
         // very simple for testing
         defender.currentHealth -= attacker.currentAttack;
     }
-    
+
     public void Death()
     {
         Events.minionDeathrattleEvent += Deathrattle;
@@ -79,86 +79,4 @@ public class BasicMinionSuperClass : MonoBehaviour {
 
 	}
     */
-=======
-﻿using UnityEngine;
-using System.Collections;
-
-public class BasicMinionSuperClass : MonoBehaviour {
-
-	public int baseHealth ;
-	public int baseAttack ;
-	public int currentAttack ;
-	public int currentHealth ;
-	public int baseManaCost ;
-    public int currentManaCost;
-	public string TAG ;
-
-    public bool hasSpellImmunity;
-	public bool hasDivinieShield ;
-	public bool hasWindfury ;
-	public bool hasStealth ;
-	public bool hasCharge ;
-    public bool hasTaunt;
-    public bool hasMegaWindfury;
-    public bool hasMegaDivineShield;
-
-	public bool hasTargetAbility ;
-	public bool canTargetAllies ;
-	public bool canTargetEnemies ;
-	public bool canTargetHeroes ;
-	public bool isFrozen ;
-
-    void OnEnable()
-    {
-        Events.minionDealDamageEvent += DealDamage;
-    }
-
-    void OnDisable()
-    {
-        Events.minionDealDamageEvent -= DealDamage;
-    }
-
-    public void DealDamage(BasicMinionSuperClass attacker, BasicMinionSuperClass defender)
-    {
-        // very simple for testing
-        defender.currentHealth -= attacker.currentAttack;
-    }
-    
-    public void Death()
-    {
-        Events.minionDeathrattleEvent += Deathrattle;
-    }
-
-    public void Deathrattle()
-    {
-        // do deathrattle stuff
-        Events.minionDeathrattleEvent -= Deathrattle;
-    }
-
-    /* old guy stuff
-	void OnEnable () {
-
-	}
-
-	void OnDisable () {
-		// stop listening the methods
-
-	}
-
-	public virtual void OnSummon () {
-
-	}
-
-	public virtual void OnBattleCry () {
-		BattleCry[] btcries =  (BattleCry[]) GetComponents<BattleCry> ();
-		foreach ( BattleCry bc in btcries ){
-			bc.OnBattleCry () ;
-		}
-	}
-
-	public virtual void OnDeathRattle () {
-
-	}
-    */
->>>>>>> origin/master:assets/scripts/CardScripts/BasicMinionSuperClass.cs
 }
