efbfc3136d0d34932f2e2a9c2b53e05c9f987232
Snake <urimr26cat@gmail.com>
2016-03-25 22:22:03 +0100

    Code conventions and commenting for BoardManager and adding left arrow

--- assets/Hearthstones/Scripts/Managers/BoardManager.cs
+++ assets/Hearthstones/Scripts/Managers/BoardManager.cs
@@ -1,26 +1,56 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.SceneManagement;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
-public class BoardManager : MonoBehaviour {
-
-	public List<GameObject> maps = new List<GameObject>();
-	public GameObject currentMap;
+public class BoardManager : MonoBehaviour
+{
+	public List<GameObject> Maps = new List<GameObject>();
+	public GameObject CurrentMap;
+    
 	private int _mapNumber = 0;
-	private bool _changeToZero = false;
 
-	void Update () {
-		if(_mapNumber == 7) {
-			_changeToZero = true;
-		}
-		if(Input.GetKeyDown(KeyCode.RightArrow)) {
-			Destroy (currentMap);
-			currentMap = Instantiate (maps [_mapNumber++]);
-		}
-		if(_changeToZero == true) {
-			_mapNumber = 0;
-			_changeToZero = false;
-		}
-	}
+	private void Update()
+    {
+        // Checking if the user has pressed the Left Arrow
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (_mapNumber == 0)
+            {
+                // Returning to the end of the list
+                _mapNumber = 6;
+            }
+            else
+            {
+                // Substracting 1 to the map number count
+                _mapNumber--;
+            }
+
+            SwitchMap();
+        }
+
+        // Checking if the user has pressed the Right Arrow
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (_mapNumber == 6)
+            {
+                // Returning to the end of the list
+                _mapNumber = 0;
+            }
+            else
+            {
+                // Adding 1 to the map number count
+                _mapNumber++;
+            }
+
+            SwitchMap();
+        }
+    }
+
+    private void SwitchMap()
+    {
+        // Destroying the current map
+        Destroy(CurrentMap);
+
+        // Instantiating a new map
+        CurrentMap = Instantiate(Maps[_mapNumber]);
+    }
 }
