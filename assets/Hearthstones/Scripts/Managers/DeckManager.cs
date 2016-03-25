058b1b5789e628ea11bcef33a1b0dab168ec56e2
Snake <urimr26cat@gmail.com>
2016-03-25 22:29:12 +0100

    Fixing indents from visual studio

--- assets/Hearthstones/Scripts/Managers/DeckManager.cs
+++ assets/Hearthstones/Scripts/Managers/DeckManager.cs
@@ -8,22 +8,22 @@ public class DeckManager : MonoBehaviour
 
 	private List<GameObject> _currentDeck = new List<GameObject>();
 
-	private void Start()
+    private void Start()
     {
         // Looping on all cards of the Deck
-		for (int i = 0; i < Deck.transform.childCount; i++)
+        for (int i = 0; i < Deck.transform.childCount; i++)
         {
             // Adding the card to the current deck
-			_currentDeck.Add(Deck.transform.GetChild(i).gameObject);
+            _currentDeck.Add(Deck.transform.GetChild(i).gameObject);
 		}
 
         // Shuffling all the cards in the deck
-		for (int t = 0; t < _currentDeck.Count; t++)
+        for (int t = 0; t < _currentDeck.Count; t++)
         {
-			GameObject tmp = _currentDeck[t];
-			int r = Random.Range(t, _currentDeck.Count);
-			_currentDeck[t] = _currentDeck[r];
-			_currentDeck[r] = tmp;
+		    GameObject tmp = _currentDeck[t];
+	        int r = Random.Range(t, _currentDeck.Count);
+	        _currentDeck[t] = _currentDeck[r];
+	        _currentDeck[r] = tmp;
 		}
 
         // Drawing the 3 starting cards
