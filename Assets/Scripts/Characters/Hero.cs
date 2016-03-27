f9fc26aa3867f9d17ded8252b93d9acfbec1667c
Snake <urimr26cat@gmail.com>
2016-03-27 22:07:56 +0200

    Fixing indents on Hero

--- Assets/Scripts/Characters/Hero.cs
+++ Assets/Scripts/Characters/Hero.cs
@@ -5,18 +5,18 @@ public class Hero : MonoBehaviour, IDamageable
     // Stats //
     public int Attack = 0;
     public int BaseHP = 30;
-	public int CurrentHp;
+    public int CurrentHp;
     public int Armor = 0;
 
     // Effects //
     public bool Frozen = false;
-	public bool Immune = false;
+    public bool Immune = false;
     public bool Forgetful = false;
 
-	private void Start()
-	{
-        
-	}
+    private void Start()
+    {
+
+    }
 
     public void Damage(int damageAmount)
     {
