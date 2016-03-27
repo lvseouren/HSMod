c1f91ea2300e85ac9db9ecdd3f850dece1b87da4
Snake <urimr26cat@gmail.com>
2016-03-27 18:01:10 +0200

    Adding DeathKnight card folder and example

---
+++ Assets/Scripts/Cards/UniqueCards/DeathKnight/SkeletonCommander.cs
@@ -0,0 +1,22 @@
+﻿public class SkeletonCommander : MinionCard
+{
+    public SkeletonCommander()
+    {
+        Name = "Skeleton Commander";
+        Description = "Deathrattle: The next Undead you play gets +1/+1.";
+
+        CardClass = CardClass.DeathKnight;
+        Rarity = Rarity.Common;
+        MinionType = MinionType.Undead;
+
+        Cost = 1;
+        BaseAttack = 1;
+        CurrentAttack = 1;
+        Health = 1;
+    }
+
+    public override void OnDead()
+    {
+        // TODO : The next Undead you play gets +1/+1.
+    }
+}
