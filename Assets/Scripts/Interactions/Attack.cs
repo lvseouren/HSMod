f5574e210c3977b5e3f058141921e92cc12562ca
Snake <urimr26cat@gmail.com>
2016-03-27 20:14:10 +0200

    Fixing indents

--- Assets/Scripts/Interactions/Attack.cs
+++ Assets/Scripts/Interactions/Attack.cs
@@ -3,11 +3,11 @@ using System.Collections;
 
 public class Attack : MonoBehaviour
 {
-	public GameObject seta;
+    public GameObject seta;
     public GameObject Ready;
     public LineRenderer linha;
 
-	public bool canTargetAllies;
+    public bool canTargetAllies;
     public bool canTargetHero;
     public bool canTargetOponents;
     public bool isSleeping;
@@ -16,7 +16,7 @@ public class Attack : MonoBehaviour
 
     public int AttackCounter;       //Counting how many Attacks are left for a minion
     public int DivineCounter;       //Counting the Divine shields a minion has left
-    
+
     private Vector3 screenPoint;
     private Vector3 curScreenPoint;
     private Vector3 curPosition;
@@ -43,11 +43,11 @@ public class Attack : MonoBehaviour
         {
             DivineCounter = 0;
         }
-        
-		if (GetComponent<BasicMinionSuperClass> ().hasCharge)
+
+        if (GetComponent<BasicMinionSuperClass>().hasCharge)
         {
-			isSleeping = false;
-            
+            isSleeping = false;
+
             if (GetComponent<BasicMinionSuperClass>().hasWindfury)
             {
                 AttackCounter = 2;
@@ -60,10 +60,10 @@ public class Attack : MonoBehaviour
         }
         else
         {
-			isSleeping = true ;
+            isSleeping = true;
             AttackCounter = 0;
-		}
-	}
+        }
+    }
 
     private void Start()
     {
@@ -81,7 +81,7 @@ public class Attack : MonoBehaviour
             {
                 //Green glow gets added here
             }
-            
+
             if (GetComponent<BasicMinionSuperClass>().hasMegaWindfury)
             {
                 AttackCounter = 4;
@@ -106,37 +106,9 @@ public class Attack : MonoBehaviour
         }
     }
 
-	private void OnMouseDown()
+    private void OnMouseDown()
     {
-		Debug.Log("Failing");
-
-		if (isSleeping)
-			return;
-
-        if (notYourTurn)
-            return;
-
-        if (AttackCounter <= 0)
-            return;
-        
-		Debug.Log("Downing");
-
-		linha.enabled = true;
-
-		screenPoint = Camera.main.WorldToScreenPoint(transform.position);
-		curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-		curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
-
-		seta.transform.position = curPosition;
-
-		linha.SetPosition(0, new Vector3(this.transform.position.x, 10f, this.transform.position.z));
-		linha.SetPosition(1, curPosition);
-	}
-
-	private void OnMouseDrag()
-    {
-		// if (alreadyAttackedThisTurn)
-		//	return;
+        Debug.Log("Failing");
 
         if (isSleeping)
             return;
@@ -147,68 +119,96 @@ public class Attack : MonoBehaviour
         if (AttackCounter <= 0)
             return;
 
-		Debug.Log("Dragging");
-        
+        Debug.Log("Downing");
+
+        linha.enabled = true;
+
+        screenPoint = Camera.main.WorldToScreenPoint(transform.position);
         curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-		curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+        curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
 
-		linha.SetPosition(1, curPosition);
-	}
+        seta.transform.position = curPosition;
 
-	private void OnMouseUp()
+        linha.SetPosition(0, new Vector3(this.transform.position.x, 10f, this.transform.position.z));
+        linha.SetPosition(1, curPosition);
+    }
+
+    private void OnMouseDrag()
     {
-		if (isSleeping)
-			return;
-        
+        // if (alreadyAttackedThisTurn)
+        //	return;
+
+        if (isSleeping)
+            return;
+
         if (notYourTurn)
             return;
-        
+
         if (AttackCounter <= 0)
             return;
 
-		Debug.Log("Upping");
-        
+        Debug.Log("Dragging");
+
+        curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+
+        linha.SetPosition(1, curPosition);
+    }
+
+    private void OnMouseUp()
+    {
+        if (isSleeping)
+            return;
+
+        if (notYourTurn)
+            return;
+
+        if (AttackCounter <= 0)
+            return;
+
+        Debug.Log("Upping");
+
         linha.enabled = false;
 
-		curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-		curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+        curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
 
-		RaycastHit[] hits = Physics.RaycastAll(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector3.down, 1000f);
-		bool played = false ;
-        
-		for (int i = 0; i < hits.Length; i++)
+        RaycastHit[] hits = Physics.RaycastAll(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector3.down, 1000f);
+        bool played = false;
+
+        for (int i = 0; i < hits.Length; i++)
         {
-			if (hits[i].transform.name == "TheirHero")
+            if (hits[i].transform.name == "TheirHero")
             {
-				StartCoroutine(attack(hits[i].transform.position, hits[i].transform.gameObject));
-			}
-            else if (hits[i].transform.parent!= null && hits[i].transform.parent.name == "TheirSideOfBf")
+                StartCoroutine(attack(hits[i].transform.position, hits[i].transform.gameObject));
+            }
+            else if (hits[i].transform.parent != null && hits[i].transform.parent.name == "TheirSideOfBf")
             {
-				StartCoroutine(attack(hits[i].transform.position, hits[i].transform.gameObject));
-			}
-		}
-	}
+                StartCoroutine(attack(hits[i].transform.position, hits[i].transform.gameObject));
+            }
+        }
+    }
 
-	private IEnumerator attack(Vector3 finalPosition, GameObject hit)
+    private IEnumerator attack(Vector3 finalPosition, GameObject hit)
     {
-		finalPosition.z -= GetComponent<BoxCollider>().bounds.size.z/2;
-		Vector3 initialPosition = this.transform.position;
+        finalPosition.z -= GetComponent<BoxCollider>().bounds.size.z / 2;
+        Vector3 initialPosition = this.transform.position;
 
-	    Vector2 firstPoint = new Vector2(this.transform.position.x, this.transform.position.z);
-	    Vector2 secondPoint = new Vector2(finalPosition.x, finalPosition.z);
+        Vector2 firstPoint = new Vector2(this.transform.position.x, this.transform.position.z);
+        Vector2 secondPoint = new Vector2(finalPosition.x, finalPosition.z);
 
-	    float Distance = Vector2.Distance(firstPoint, secondPoint);
+        float Distance = Vector2.Distance(firstPoint, secondPoint);
 
-		while (Vector3.Distance(this.transform.position, finalPosition) > 0.01f)
+        while (Vector3.Distance(this.transform.position, finalPosition) > 0.01f)
         {
-			this.transform.position = Vector3.MoveTowards(this.transform.position, finalPosition, Distance * Time.deltaTime * 10f);
-			yield return null;
-		}
+            this.transform.position = Vector3.MoveTowards(this.transform.position, finalPosition, Distance * Time.deltaTime * 10f);
+            yield return null;
+        }
 
         if (hit.GetComponent<Hero>() != null)
         {
             hit.GetComponent<Hero>().currentHp -= GetComponent<BasicMinionSuperClass>().currentAttack;
-            hit.GetComponent<Hero>().attack -= GetComponent<BasicMinionSuperClass>().currentHealth; 
+            hit.GetComponent<Hero>().attack -= GetComponent<BasicMinionSuperClass>().currentHealth;
         }
 
         if (hit.GetComponent<BasicMinionSuperClass>() != null)
@@ -237,6 +237,6 @@ public class Attack : MonoBehaviour
                 this.transform.position = Vector3.MoveTowards(this.transform.position, initialPosition, Distance * Time.deltaTime * 10);
                 yield return null;
             }
-		}
-	}
+        }
+    }
 }
