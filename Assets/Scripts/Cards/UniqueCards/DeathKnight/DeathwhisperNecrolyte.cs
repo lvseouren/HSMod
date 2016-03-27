13bb60079e27caeee4ab8dc9d8eeffbac115805b
FullasGames <Saldez@hotmail.co.uk>
2016-03-27 17:10:50 +0100

    Create DeathwhisperNecrolyte.cs

---
+++ Assets/Scripts/Cards/UniqueCards/DeathKnight/DeathwhisperNecrolyte.cs
@@ -0,0 +1,22 @@
+public class DeathwhisperNecrolyte : MinionCard
+{
+    public DeathwhsiperNecrolyte()
+    {
+        Name = "Deathwhsiper Necrolyte";
+        Description = "Your other Undead have +1 Attack.";
+
+        CardClass = CardClass.DeathKnight;
+        Rarity = Rarity.Common;
+        MinionType = MinionType.Undead;
+
+        Cost = 2;
+        BaseAttack = 1;
+        CurrentAttack = 1;
+        Health = 4;
+    }
+
+    public override void OnDead()
+    {
+        // TODO : +1 Attack Aura
+    }
+}
