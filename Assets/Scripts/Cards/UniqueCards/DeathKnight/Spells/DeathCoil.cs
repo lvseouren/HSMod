273c4f7294af1cc7de35e184c56d6f565ae6ea06
FullasGames <Saldez@hotmail.co.uk>
2016-03-28 19:45:04 +0100

    Create DeathCoil.cs

---
+++ Assets/Scripts/Cards/UniqueCards/DeathKnight/Spells/DeathCoil.cs
@@ -0,0 +1,22 @@
+public class DeathCoil : SpellCard
+{
+    public DeathCoil()
+    {
+        Name = "Death Coil";
+        Description = "Deal 2 damage to a minion. If it's a friendly Undead, restore it to full Health instead.";
+
+        CardClass = CardClass.DeathKnight;
+        Rarity = Rarity.Common;
+
+        Cost = 1;
+    }
+
+    public override void Cast(IDamageable target)
+    {
+        //EventManager.OnSpellPreCast();
+
+        target.Damage(2 + this.Player.SpellDamage);
+
+        //EventManager.OnSpellCasted();
+    }
+}
