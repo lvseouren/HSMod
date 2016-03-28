0079cc2f8c80a9e142ad19d7c81f26da8ffca9cf
Snake <urimr26cat@gmail.com>
2016-03-28 15:54:33 +0200

    DeathKnight spell example

---
+++ Assets/Scripts/Cards/UniqueCards/DeathKnight/Spells/AllWillServe.cs
@@ -0,0 +1,22 @@
+﻿public class AllWillServe : SpellCard
+{
+    public AllWillServe()
+    {
+        Name = "All Will Serve";
+        Description = "Deal 2 damage. Summon a 1/1 Ghoul with Charge.";
+
+        CardClass = CardClass.DeathKnight;
+        Rarity = Rarity.Common;
+
+        Cost = 2;
+    }
+
+    public override void Cast(IDamageable target)
+    {
+        //EventManager.OnSpellPreCast();
+
+        target.Damage(2 + this.Player.SpellDamage);
+
+        //EventManager.OnSpellCasted();
+    }
+}
