a7edb2fd3f8e8470a02c350fdb2274eddec949ce
Snake <urimr26cat@gmail.com>
2016-04-13 21:38:12 +0200

    First commit of the DeathKnight Hero Power

---
+++ Assets/Scripts/HeroPowers/UniqueHeroPowers/RaiseGhoul.cs
@@ -0,0 +1,18 @@
+﻿public class RaiseGhoul : BaseHeroPower
+{
+    public Hero Hero;
+
+    public RaiseGhoul(Hero hero)
+    {
+        Name = "Raise Ghoul";
+        Description = "Summon a 1/1 Ghoul with Charge that dies at end of turn.";
+        Class = HeroClass.DeathKnight;
+
+        Hero = hero;
+    }
+
+    public override void Use()
+    {
+        // TODO : Summon 1/1 Ghoul
+    }
+}
