222ebd56d78c757dc20c53b611be80421cdf5fa2
Snake <urimr26cat@gmail.com>
2016-04-13 18:32:22 +0200

    Starting the saving system for Decks

---
+++ Assets/Scripts/DeckBuilder/SavedCard.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+[Serializable]
+public class SavedCard
+{
+    public string Name;
+    public bool Golden;
+
+    public SavedCard(string name, bool golden)
+    {
+        Name = name;
+        Golden = golden;
+    }
+
+    public BaseCard ToCard()
+    {
+        // Getting the Type of the card based on the name
+        Type cardType = Type.GetType(Name);
+
+        if (cardType != null)
+        {
+            return (BaseCard) Activator.CreateInstance(cardType);
+        }
+        return null;
+    }
+}
