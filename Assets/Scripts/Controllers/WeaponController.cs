31c08da58dad8807c169fb485c31a41e112bb2b2
Snake <urimr26cat@gmail.com>
2016-04-27 14:49:51 +0200

    Adding new WeaponController

---
+++ Assets/Scripts/Controllers/WeaponController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class WeaponController : BaseController
+{
+    public WeaponCard Weapon;
+
+    public SpriteRenderer WeaponRenderer;
+    public SpriteRenderer TokenRenderer;
+
+    public TextMesh AttackText;
+    public TextMesh DurabilityText;
+
+    public static WeaponController Create(WeaponCard weapon, Vector3 weaponPosition)
+    {
+        GameObject heroObject = new GameObject("Weapon_" + weapon.Name);
+        heroObject.transform.position = weaponPosition;
+        heroObject.transform.localScale = new Vector3(50f, 50f, 50f);
+        heroObject.transform.localEulerAngles = new Vector3(90f, 0f, 0f);
+
+        SphereCollider weaponCollider = heroObject.AddComponent<SphereCollider>();
+
+        WeaponController weaponController = heroObject.AddComponent<WeaponController>();
+        weaponController.Weapon = weapon;
+
+        weaponController.Initialize();
+
+        return weaponController;
+    }
+}
