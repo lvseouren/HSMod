a294ad3c622a8abfb5a5855255bca5540e81f94c
saikrishnawanted <saikrishnawanted@gmail.com>
2016-05-19 18:32:32 +0530

    Revert "removing junk"
    This reverts commit 3ba3f7841aa122424229e76f35349358a5ed087d.

---
+++ Assets/NewItems/Scripts/Drag.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Drag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+	public Transform parentToReturnTo = null;
+
+	public void OnBeginDrag(PointerEventData eventData){
+		Debug.Log("OnBeginDrag");
+		parentToReturnTo = this.transform.parent;
+		this.transform.SetParent (this.transform.parent.parent);
+		GetComponent<CanvasGroup> ().blocksRaycasts = false;
+	}
+	public void OnDrag(PointerEventData eventData){
+		Debug.Log("Drag");
+		this.transform.position = eventData.position;
+	}
+	public void OnEndDrag(PointerEventData eventData){
+		Debug.Log("EndDrag");
+		this.transform.SetParent (parentToReturnTo);
+		GetComponent<CanvasGroup> ().blocksRaycasts = true;
+	}
+}
