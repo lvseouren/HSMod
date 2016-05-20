7b5ae010ec3fbd8ea125263d811a35286de62530
Snake <urimr26cat@gmail.com>
2016-05-20 02:50:52 +0200

    Adding scripts for missing neutral minions

---
+++ Assets/Scripts/Cards/UniqueCards/Neutral/ArcaneAnomaly.cs
@@ -0,0 +1,20 @@
+﻿public class ArcaneAnomaly : MinionCard
+{
+    public ArcaneAnomaly()
+    {
+        Name = "Arcane Anomaly";
+        Description = "Whenever you spend mana, deal 1 damage to a random enemy.";
+
+        Class = HeroClass.Neutral;
+        Rarity = CardRarity.Rare;
+        MinionType = MinionType.General;
+
+        BaseCost = 2;
+        BaseAttack = 2;
+        BaseHealth = 1;
+
+        InitializeMinion();
+    }
+
+    // TODO
+}
