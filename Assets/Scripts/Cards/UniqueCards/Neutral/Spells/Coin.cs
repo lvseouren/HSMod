9934836910195bd2da959a8ec8923d6c6fa7cf7d
Snake <urimr26cat@gmail.com>
2016-05-24 08:22:43 +0200

    Adding coin script

---
+++ Assets/Scripts/Cards/UniqueCards/Neutral/Spells/Coin.cs
@@ -0,0 +1,22 @@
+﻿public class Coin : SpellCard
+{
+    public Coin()
+    {
+        Name = "Coin";
+        Description = "Gain 1 Mana Crystal this turn only.";
+
+        Class = HeroClass.Neutral;
+        Rarity = CardRarity.Basic;
+
+        BaseCost = 0;
+
+        TargetType = TargetType.NoTarget;
+
+        InitializeSpell();
+    }
+
+    public override void Cast(Character target)
+    {
+        Player.AddMana(1);
+    }
+}
